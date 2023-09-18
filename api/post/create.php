073e54a8143f3776261e5b283603b2ecaadea19e
kriztiank <kriztiank@gmail.com>
2023-09-18 07:08:04 +0200

    Delete Post

--- api/post/create.php
+++ api/post/create.php
@@ -1,37 +1,36 @@
 <!-- http://localhost/php_rest_myblog/api/post/create.php -->
 
-<?php 
-  // Headers
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: POST');
-  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+<?php
+// Headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST");
+header(
+    "Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With"
+);
 
-  include_once '../../config/Database.php';
-  include_once '../../models/Post.php';
+include_once "../../config/Database.php";
+include_once "../../models/Post.php";
 
-  // Instantiate DB & connect
-  $database = new Database();
-  $db = $database->connect();
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
 
-  // Instantiate blog post object
-  $post = new Post($db);
+// Instantiate blog post object
+$post = new Post($db);
 
-  // Get raw posted data from the input
-  $data = json_decode(file_get_contents("php://input"));
+// Get raw posted data from the input
+$data = json_decode(file_get_contents("php://input"));
+// Set properties/fields to add
+$post->title = $data->title;
+$post->body = $data->body;
+$post->author = $data->author;
+$post->category_id = $data->category_id;
 
-  $post->title = $data->title;
-  $post->body = $data->body;
-  $post->author = $data->author;
-  $post->category_id = $data->category_id;
+// Create post
+if ($post->create()) {
+    echo json_encode(["message" => "Post Created"]);
+} else {
+    echo json_encode(["message" => "Post Not Created"]);
+}
 
-  // Create post
-  if($post->create()) {
-    echo json_encode(
-      array('message' => 'Post Created')
-    );
-  } else {
-    echo json_encode(
-      array('message' => 'Post Not Created')
-    );
-  }
--- api/post/delete.php
+++ api/post/delete.php
@@ -1,10 +1,10 @@
-<!-- http://localhost/php_rest_myblog/api/post/update.php -->
+<!-- http://localhost/php_rest_myblog/api/post/delete.php -->
 
 <?php
 // Headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
-header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Methods: DELETE");
 header(
     "Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With"
 );
@@ -24,16 +24,10 @@ $data = json_decode(file_get_contents("php://input"));
 
 // Set ID to update
 $post->id = $data->id;
-// Set properties/fields to update
-$post->title = $data->title;
-$post->body = $data->body;
-$post->author = $data->author;
-$post->category_id = $data->category_id;
 
-// Update post
-if ($post->update()) {
-    echo json_encode(["message" => "Post Updated"]);
+// Delete post
+if ($post->delete()) {
+    echo json_encode(["message" => "Post Deleted"]);
 } else {
-    echo json_encode(["message" => "Post Not Updated"]);
+    echo json_encode(["message" => "Post Not Deleted"]);
 }
-
